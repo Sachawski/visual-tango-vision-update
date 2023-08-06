ea7500fca08e0fc0b559d016adb639acf112c2e1
sacha <swojciechowski@insa-rouen.fr>
2023-08-06 22:36:51 +0200

    nettoyage src

--- src/animation_creation.py
+++
@@ -1,9 +0,0 @@
-import imageio
-import os
-# List all files in the directory with the .png extension
-image_files = sorted([f for f in os.listdir() if f.endswith('.png')])
-
-# Read each file into an array
-images = [imageio.imread(f) for f in image_files]
-# Save the images as an animation
-imageio.mimsave('animation2.mp4', images, fps=30)
